2a163bb501be171fe6ad9f7cde5a6b7983d346f4
ninjatill <ninjatill@yahoo.com>
2018-03-27 16:47:13 -0400

    Update wpProQuiz_toplist.js
    Fixed count variable in td(0).

--- js/wpProQuiz_toplist.js
+++ js/wpProQuiz_toplist.js
@@ -37,7 +37,7 @@ function wpProQuiz_fetchToplist() {
                             }
                         }
                         
-                        td.eq(0).text(i + 1);
+                        td.eq(0).text(count);
                         td.eq(1).text(data[i].name);
                         td.eq(2).text(data[i].date);
                         td.eq(3).text(attempts);
